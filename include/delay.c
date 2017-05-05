f142fce16466342bc6e519cdeaed4d13e0a6fc7e
Rusty Mina <rusty.mina@eee.upd.edu.ph>
2017-05-06 01:27:10 +0800

    Added delay library

---
+++ include/delay.c
@@ -0,0 +1,36 @@
+/* 
+ * File: delay.c  
+ * Author: Rusty Mina (rusty.mina@eee.upd.edu.ph)
+ * Comments:
+ * Revision history: 1
+ * Note: If you want to change the clock frequency
+ * change it in delay_cfg.h
+ */
+
+void delay(int delay_constant);
+void delay_ms(int ms);
+void delay_us(int us);
+
+// delay()
+void delay(int delay_constant){
+    int count, count2;   
+    for (count2 = 0; count2 < delay_constant; count2++){
+        for (count = 0; count < DELAY_ITERATION; count++){
+        Nop();
+        Nop();
+        }
+    }
+    return;
+}
+
+// delay_ms()
+void delay_ms(int ms){
+    __delay32(CYCLES_PER_MS * ((unsigned long) ms));   //__delay32 is provided by the compiler, delay some # of milliseconds
+    return;
+}
+
+// delay_us();
+void delay_us(int us){
+    __delay32(CYCLES_PER_US * ((unsigned long) us));    //delay some number of microseconds
+    return;
+}
---
+++ include/delay_cfg.h
@@ -0,0 +1,51 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File: delay_cfg.h  
+ * Author: Rusty Mina (rusty.mina@eee.upd.edu.ph)
+ * Comments:
+ * Revision history: 1
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef __delay_cfg_____
+#define	__delay_cfg_____
+#endif
+
+#include <xc.h> // include processor files - each processor file is guarded.
+
+#ifndef FCY
+#define	FCY 4000000UL
+#endif
+
+#ifndef DELAY_ITERATION
+#define DELAY_ITERATION 498
+#endif
+
+#ifndef CYCLES_PER_US
+#define CYCLES_PER_US ((unsigned long)(FCY * 0.000001))   //instruction cycles per microsecond
+#endif 
+
+#ifndef CYCLES_PER_US
+#define CYCLES_PER_MS ((unsigned long)(FCY * 0.001))        //instruction cycles per millisecond
+#endif
