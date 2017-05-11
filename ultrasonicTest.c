254a5dbc004acc145a9cfc66c069f07e9f9bf8d4
John Cabuyadao <jbcabuyadao@up.edu.ph>
2017-05-12 00:30:46 +0800

    added some rtcc shit to ultrasonic test

--- ultrasonicTest.c
+++ ultrasonicTest.c
@@ -15,13 +15,28 @@
 #include "xc.h"
 #include "include/delay.h"
 #include "include/lcd_4bit.h"
-
+#include "include/local_rtcc.h"
 void getHigh(void);
 void getLow(void);
 void theresCar(short state);
 
+int parked = 0;//flag if a car is parked
 int count; 
-
+rtcc_t test2;
+void initTime()
+{
+     rtcc_t test;
+     rtccInit();
+    // February 18, 2017 1:13 Saturday
+    test.rtcc_hour = 1;
+    test.rtcc_mday = 18;
+    test.rtcc_min = 13;
+    test.rtcc_mon = 2;
+    test.rtcc_sec = 35;
+    test.rtcc_wday = 6;
+    test.rtcc_year = 17;
+    setTime(test);
+}
 int main(void) {
     TRISA = 0xFFFF;
     TRISB = 0;
@@ -30,14 +45,38 @@ int main(void) {
     lcdPrint("helloworld");
     setCursor(0xC0);
     PORTA = 0xFFFF;
-
+    initTime();
     delay(1000);
-    while(1){
+    /*while(1){
         clearLine2(); 
         getHigh();
         count = 0;
         getLow();
         lcdIntPrint(count);
+    }*/
+    if (count<2500)//car is parked
+    {
+        LATBbits.LATB15 = 0; //GREEN LED IS ON
+        LATBbits.LATB11 = 1; //RED LED IS OFF
+        
+        if(press == 0)
+        {
+            test2 = getTime();
+            //write this to i2c
+            press = 1;//set flag
+        }
+    }
+    else//space is free
+    {
+        LATBbits.LATB15 = 1; //GREEN LED IS OFF
+        LATBbits.LATB11 = 0; //RED LED IS ON
+        
+        if(press == 1)
+        {
+            test2 = getTime(); //get end time
+            //write this to i2c
+            press = 0; //clear flag
+        }
     }
             
     return 0;
