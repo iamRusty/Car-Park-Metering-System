15fe04d6e8afe7b5b1581073c1522ea4622e7dc4
Rusty Mina <rusty.mina@eee.upd.edu.ph>
2017-05-12 09:52:25 +0800

    Added eeprom write functions

--- main.c
+++ main.c
@@ -11,18 +11,26 @@
 #pragma config SOSCSEL=IO
 #pragma config I2C1SEL=PRI 
 
-
 #include "xc.h"
 #include "include/delay.h"
 #include "include/lcd_4bit.h"
 #include "include/keypad.h"
+#include <stdio.h>
+#include "include/i2c.h"
 
 void getHigh(void);
 void getLow(void);
 void theresCar(short state);
+void eeTime(long data, int address);
+void intToFloat(long i);
+void floatToInt(float f);
+
+union fi_t{
+    float f;
+    long i;
+};
 
 int count; 
-
 int main(void) {
     TRISA = 0xFFFF;
     TRISB = 0;
@@ -33,30 +41,14 @@ int main(void) {
     setCursor(0xC0);
     PORTA = 0xFFFF;
 
-#if 1
+#if 0
     while(1){
         LATBbits.LATB11 = 1;
         LATBbits.LATB15 = 0;
         delay(2000);
         LATBbits.LATB11 = 0;
         LATBbits.LATB15 = 1;
-        delay(2000);
-       
-        // Reset Pulldown
-        keyOff();
-        resetPullup();
-        keyOn(); 
-        
-        if(getIsPressed()) {
-            setCursor(0xc0);
-        lcdPrint("he");
-        
-            lcdIntPrint(getKeyValue());
-            setIsPressed(0);
-        }
-        else
-            continue;
-            
+        delay(2000);        
     }
 #endif 
     
@@ -75,6 +67,21 @@ int main(void) {
             theresCar(0);
         
         delay(2000);
+        
+                // Reset Pulldown
+        keyOff();
+        resetPullup();
+        keyOn(); 
+        
+        if(getIsPressed()) {
+            setCursor(0xc0);
+        lcdPrint("he");
+        
+            lcdIntPrint(getKeyValue());
+            setIsPressed(0);
+        }
+        else
+            continue;
     }
             
     return 0;
@@ -101,4 +108,34 @@ void theresCar(short state){
         LATBbits.LATB11 = 0;
         LATBbits.LATB15 = 1;
     }
+}
+
+void eeData(long data, int address){
+    char *ad_ptr = &address;           // Address to be written at
+    i2c_start();                       // Start I2C
+    i2c_tx(0b10100000);                // Send EEPROM CTRL byte with Write enabled
+    i2c_tx(*(ad_ptr+1));               // Send EEPROM High Address
+    i2c_tx(*ad_ptr);                   // Send EEPROM Low Address
+    
+    char *d_ptr = &data;
+    int count2 = 0;
+    while (count2 < 4){
+        i2c_tx(*(d_ptr+count2));
+        count2++;
+    }
+    
+    i2c_stop();
+    delay(10);
+}
+
+float intToFloat(long i){
+    fi_t fi;
+    fi.i = i;
+    return fi.f;
+}
+
+long floatToInt(float f){
+    fi_t fi;
+    fi.f = f;
+    return fi.i;
 }
